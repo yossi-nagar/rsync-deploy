9f78c80a195297f6bd72db5dddd0e4aebce22df9
Yossi Nagar <42964649+yossi-nagar@users.noreply.github.com>
2020-03-26 13:29:22 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ chmod 600 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
 # sync it up"
 # sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
-sh -c "rsync "$ARGS" -e \"ssh -T -v -2 -oHostKeyAlgorithms=+ssh-dss -i $SSHPATH/key\" $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING";
+sh -c "rsync "$ARGS" -e \"ssh -T -v -2 -oIdentitiesOnly=yes -oHostKeyAlgorithms=+ssh-dss -i $SSHPATH/key\" $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING";
